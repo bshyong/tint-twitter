4583063bb10441a85fe9e8e569b0e45dc5bd6f64
Benjamin Shyong <ben.shyong@gmail.com>
2014-08-31 01:51:25 -0700

    add specs; view cleanup

--- spec/models/battle_spec.rb
+++ spec/models/battle_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Battle, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have associated hashtags when created" do
+    b = Battle.create(name: 'test', hashtag_one: 'one', hashtag_two: 'two')
+    b.hashtags.count.should_eql 2
+  end
+  it "should validate presence of required fields" do
+    Battle.new.should_not be_valid
+    # need to have hashtag names
+    Battle.new(name: 'test').should_not be_valid
+    Battle.new(name: 'test', hashtag_one: 'one').should_not be_valid
+    Battle.new(name: 'test', hashtag_one: 'one', hashtag_two: 'two').should be_valid
+  end
 end
